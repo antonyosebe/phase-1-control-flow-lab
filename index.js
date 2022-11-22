2269ada9cefee76211e8f6f13706a5be509160d8
antonyosebe <osebe.anto@gmail.com>
2022-11-22 18:33:05 +0300

    commit tree 0bab83370fb7073e1344f3d6f105b60a268441f2

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet() {
+  // Write your code here!
+  if (feet <= 400) {
+    console.log("This one is on me!");
+  } else if (feet > 200) {
+    console.log("I will gladly take your thirty bucks.");
+  }
+  else if (feet > 2500) {
+    console.log("No can do.");
+  }
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
